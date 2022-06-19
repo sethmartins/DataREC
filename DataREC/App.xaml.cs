548fa465c749ac8f87e90596e7568c3d9243b69b
Seth Martins De Almeida Sobrinho <smartins@indracompany.com>
2022-06-19 20:25:49 -0300

    Adicionar arquivos de projeto.

---
+++ DataREC/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using DataREC.Services;
+using DataREC.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DataREC
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ DataREC/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using DataREC.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace DataREC.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
